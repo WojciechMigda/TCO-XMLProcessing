9fe94f9a8b8eeb16cab40c1f846eaa5c79e04a1e
Wojciech Migda <wojtek.golf@interia.pl>
2020-11-21 01:08:21 +0100

    Traverse ptree

--- solution/app/src/parser.cpp
+++ solution/app/src/parser.cpp
@@ -7,15 +7,47 @@
 
 #include <fstream>
 #include <string>
+#include <queue>
+
 
 using namespace neither;
+using boost::property_tree::ptree;
+
+
+tags_tree_t collect_tag_stats(ptree && pt)
+{
+    tags_tree_t tags;
+
+    std::queue<std::pair<std::string, ptree>> nodes;
+    nodes.emplace("<root>", std::move(pt));
+
+    // BFS
+    while (not nodes.empty())
+    {
+        auto & [name, subtree] = nodes.front();
+        fmt::print("{} - children size: {}\n", name, subtree.size());
+
+        for (auto & [child_name, child_subtree] : subtree)
+        {
+            if (child_name == "<xmlattr>")
+            {
+                fmt::print("    node {} has {} attrs\n", name, child_subtree.size());
+                continue;
+            }
+
+            nodes.emplace(child_name, child_subtree);
+        }
+
+        nodes.pop();
+    }
+
+    return tags;
+}
 
 
 Either<std::string, tags_tree_t> parse_xml_file(std::ifstream & ifile)
 {
     using rv_type = Either<std::string, tags_tree_t>;
-
-    using boost::property_tree::ptree;
 
     ptree pt;
 
@@ -28,9 +60,5 @@ Either<std::string, tags_tree_t> parse_xml_file(std::ifstream & ifile)
         return rv_type::leftOf(fmt::format("Bad XML: {}", ex.what()));
     }
 
-    tags_tree_t tags;
-
-    ;
-
-    return rv_type::rightOf(std::move(tags));
+    return rv_type::rightOf(collect_tag_stats(std::move(pt)));
 }
