6d30e46530bb2e8aa2b9fcca19bf8bb144c8b35b
Wojciech Migda <wojtek.golf@interia.pl>
2020-11-21 11:34:29 +0100

    Add jsonization

---
+++ solution/app/src/jsonize.cpp
@@ -0,0 +1,66 @@
+#include "jsonize.hpp"
+#include "parser.hpp"
+
+#include "json/json.hpp"
+#include "neither/either.hpp"
+
+#include <vector>
+#include <iterator>
+
+using namespace neither;
+
+
+/*
+ * Example contents:
+ * {"attributes": [{"name": "a", "required": true}], "children": [{"name": "C", "required": true}]}
+ *
+ */
+filename_w_contents_t xform_record(std::pair<std::string, tag_record_t> const & kv)
+{
+    nlohmann::json j;
+
+    j["attributes"] = nlohmann::json::array_t();
+    j["children"] = nlohmann::json::array_t();
+
+    auto const & tag_record = kv.second;
+    auto const tag_count = tag_record.count;
+    auto const & children_counts = tag_record.children_counts;
+    auto const & attribute_counts = tag_record.attribute_counts;
+
+    auto count_to_json = [tag_count](auto const & kv) -> nlohmann::json
+    {
+        nlohmann::json rv;
+
+        rv["name"] = kv.first;
+        rv["required"] = (kv.second == tag_count);
+
+        return rv;
+    };
+
+    std::transform(children_counts.cbegin(), children_counts.cend(), std::back_inserter(j["children"]), count_to_json);
+
+    std::transform(attribute_counts.cbegin(), attribute_counts.cend(), std::back_inserter(j["attributes"]), count_to_json);
+
+    filename_w_contents_t rv = {kv.first, j.dump()};
+
+    return rv;
+}
+
+
+Either<std::string, std::vector<filename_w_contents_t>> jsonize(tags_tree_t && tags)
+{
+    using rv_type = Either<std::string, std::vector<filename_w_contents_t>>;
+
+    std::vector<filename_w_contents_t> rv;
+
+    try
+    {
+        std::transform(tags.cbegin(), tags.cend(), std::back_inserter(rv), xform_record);
+    }
+    catch(nlohmann::json::exception & ex)
+    {
+        return rv_type::leftOf(ex.what());
+    }
+
+    return rv_type::rightOf(rv);
+}
---
+++ solution/app/src/jsonize.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef APP_SRC_JSONIZE_HPP_
+#define APP_SRC_JSONIZE_HPP_
+
+#include "parser.hpp"
+
+#include <vector>
+#include <string>
+
+typedef struct
+{
+    std::string fname;
+    std::string contents;
+} filename_w_contents_t;
+
+neither::Either<std::string, std::vector<filename_w_contents_t>> jsonize(tags_tree_t && tags);
+
+
+#endif /* APP_SRC_JSONIZE_HPP_ */
